0f800bc89e768ca9b108334ab0048e8db0763a02
sandy11JsDs <jorgesandy965@gmail.com>
2023-09-10 11:18:03 -0500

    initial commit

---
+++ src/main/java/pe/edu/cibertec/appwebejercicio/AppWebEjercicioApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebejercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebEjercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebEjercicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/appwebejercicio/AppWebEjercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebEjercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
